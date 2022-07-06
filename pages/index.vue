7ed9c5dbda663d8b1b2463a7e6e6d2807e598e06
keith hill <keith.hill@metastock.com>
2022-07-06 17:22:32 -0600

    initial commit, tested tailwind, scss NOT WORKING

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <main class="container mx-auto bg-blue-200">
+    <h1 class="text-2xl font-bold text-red-700">what in the what?</h1>
+    <Tutorial />
+    <div class="wtw">
+      <ul>
+        <li>wtw</li>
+      </ul>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
+
+<style  scoped>
+</style>
